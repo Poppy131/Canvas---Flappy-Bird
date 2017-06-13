1c3e62c207107503f762434255495c6a363ba447
poppy <poppy@example.com>
2017-06-13 12:57:24 +0800

    修改pipr-space

--- js/pipe.js
+++ js/pipe.js
@@ -1,51 +1,51 @@
 /**
  * Created by apple on 2017/6/11.
  */
-(function(Fly){
+(function(Fly) {
 
-    function Pipe(config){
-      this.imgTop = config.imgTop;
-      this.imgBottom = config.imgBottom;
-      this.ctx = config.ctx;
-      this.x = config.x;
+    function Pipe(config) {
+        this.imgTop = config.imgTop;
+        this.imgBottom = config.imgBottom;
+        this.ctx = config.ctx;
+        this.x = config.x;
 
-      this.imgW = this.imgTop.width;
-      this.imgH = this.imgTop.height;
-      this.space = 200;
+        this.imgW = this.imgTop.width;
+        this.imgH = this.imgTop.height;
+        this.space = 150;
 
-      this.topY = 0;
-      this.bottomY = 0;
-      this.speed = 0.15;
+        this.topY = 0;
+        this.bottomY = 0;
+        this.speed = 0.15;
 
-      this.randomY();
+        this.randomY();
     }
 
     Pipe.prototype = {
 
-        constructor : Pipe,
-        draw : function(delta){
+        constructor: Pipe,
+        draw: function(delta) {
 
             this.x -= this.speed * delta;
 
-            if(this.x < -this.imgW * 3){
+            if (this.x < -this.imgW * 3) {
                 this.x += this.imgW * 3 * 6;
                 this.randomY();
             }
             //
-            this.ctx.rect(this.x,this.topY,this.imgW,this.imgH)
-            this.ctx.rect(this.x,this.bottomY,this.imgW,this.imgH)
+            this.ctx.rect(this.x, this.topY, this.imgW, this.imgH)
+            this.ctx.rect(this.x, this.bottomY, this.imgW, this.imgH)
 
             //this.ctx.fill();
 
-            this.ctx.drawImage(this.imgTop,0,0,this.imgW,this.imgH,this.x,this.topY,this.imgW,this.imgH)
-            this.ctx.drawImage(this.imgBottom,0,0,this.imgW,this.imgH,this.x,this.bottomY,this.imgW,this.imgH)
+            this.ctx.drawImage(this.imgTop, 0, 0, this.imgW, this.imgH, this.x, this.topY, this.imgW, this.imgH)
+            this.ctx.drawImage(this.imgBottom, 0, 0, this.imgW, this.imgH, this.x, this.bottomY, this.imgW, this.imgH)
         },
 
-        //Ëæ»úY×ø±ê
-        randomY: function (){
-          var num =  Math.random()*200+50;
+        //ï¿½ï¿½ï¿½Yï¿½ï¿½ï¿½ï¿½
+        randomY: function() {
+            var num = Math.random() * 200 + 50;
 
-            this.topY = num + this.space ;
+            this.topY = num + this.space;
             this.bottomY = num - this.imgH;
         }
     }
